d8225f356580b14e7aa92c34cb8d8464bdc83785
Takish Lim <rimutakeshi155@gmail.com>
2024-07-25 09:17:59 +0900

    improved ui experience by adding loading when page loads on other page and try to add manifest

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,11 +1,10 @@
 import prisma from "@/app/api/db";
 import ProductPageSkeleton from "@/components/skeleton/product-page-skeleton";
+import Loading from "../product/loading";
+import LoadingSpinner from "../loading";
 
 const Page = async () => {
-  const product = await prisma.product.findUnique({
-    where: { slug: "amd_ryzen_7_7800x3d" },
-  });
-  return <p>{product?.productPrice}</p>;
+  return <LoadingSpinner />;
 };
 
 export default Page;
